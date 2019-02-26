90bb41e3f0666a184756a96f0974670b7697bd8a
David Nadeau <nadeaud.d@gmail.com>
2019-02-26 11:44:46 -0700

    updates

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public int length = 10;
+    public int width = 10;
+    public float cellSize = 2.5f;
+    public float platformHeight = -1.0f;
+    public GameObject platformCell;
+    public GameObject gridParent;
+
+    public void Build3DGrid(){
+        ClearGrid();
+        for (int y = 0; y < length; y++) 
+        {
+            for (int x = 0; x < width; x++)
+            {
+                Vector3 pos = new Vector3(x, platformHeight, y) * cellSize;
+                CreatePlatformObject(pos);
+            }
+        }
+    }
+    private void ClearGrid(){
+        foreach (Transform child in gridParent.transform) {
+        GameObject.Destroy(child.gameObject);
+ }
+    }
+    private void CreatePlatformObject(Vector3 position) {
+        GameObject newObj;
+        newObj = Instantiate(platformCell, position, Quaternion.identity);
+        newObj.transform.parent = gridParent.transform;
+    }
+
+}
---
+++ Assets/UIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject loseScreen;
+	public CanDie player;
+
+    void Start(){
+        
+		player.isDead += PlayerDied;
+    }
+    
+    void PlayerDied() {
+        ShowLoseScreen();
+	}
+
+    void ShowLoseScreen() {
+        loseScreen.SetActive(true);
+    }
+}
